635e5eedfc135a86a32b6cf83e33dd2a8a690f6c
Mohammed Mustapha Kura <mustykmohammed@gmail.com>
2023-06-03 16:03:58 +0530

    Added basic code for Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,10 @@
+import "./Card.css"
 export default function Card(){
     return(
         <>
-        
+            <img src="./src/assets/icon-sedans.svg"/>
+            <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
+            <button>Learn More</button>
         </>
     )
 }
