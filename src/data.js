552332c3e0a9be3894ca1d8f15b38687fd9d1c21
Mohammed Mustapha Kura <mustykmohammed@gmail.com>
2023-06-03 15:54:15 +0530

    Added id key to data.js

--- src/data.js
+++ src/data.js
@@ -1,13 +1,16 @@
 export default [
     {
+        id: 1,
         carModel:"Sedan",
         description:"Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
     },
     {
+        id: 2,
         carModel:"SUVs",
         description:"Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
     },
     {
+        id: 3,
         carModel:"Luxury",
         description:"Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
     }
