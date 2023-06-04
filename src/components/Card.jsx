cfaf8a2c9cb12301b10d1cecaee86428a4aa2eaf
Mohammed Mustapha Kura <mustykmohammed@gmail.com>
2023-06-04 12:13:38 +0530

    Added basic styling for Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 import "./Card.css"
 export default function Card(){
     return(
-        <>
+        <div className="card--info">
             <img src="./src/assets/icon-sedans.svg"/>
+            <h2>SEDANS</h2>
             <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
             <button>Learn More</button>
-        </>
+        </div>
     )
 }
